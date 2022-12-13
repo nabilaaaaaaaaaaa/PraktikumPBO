fa3283f7fe7ba85d4e64a3f0af356469db9d1c1e
nabila ananda masya azahra <HP@LAPTOP-K3KAMVQT>
2022-12-13 11:41:29 +0700

    yaudahla ya

---
+++ src/main/java/com/nabilaanandamasyaazahra/praktikumpbo/pertemuan8/User.java
@@ -0,0 +1,17 @@
+package com.nabilaanandamasyaazahra.praktikumpbo.pertemuan8;
+public class User {
+    String email;
+    String password;
+    
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+    
+    public boolean verify(String email, String password) {
+        if (this.email.equals(email) && this.password.equals(password))
+            return true;
+        
+        return false;
+    }
+}
